071767be8ca4b5e1144c03260996fe6ba7ac762e
Jacob Powell <Scout2012>
2020-12-08 03:05:17 -0800

    removed unneeded json.dumps call

--- music-backend/main.py
+++ music-backend/main.py
@@ -12,7 +12,6 @@ def get_playlist_titles(playlist: str) -> List[str]:
     data = re.findall(pattern, source)[0]
 
     playlist_videos = json.loads(data)['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['playlistVideoListRenderer']['contents']
-    # print(json.dumps(playlist_videos, indent=2))
 
     for video in playlist_videos:
         video_names.append(video['playlistVideoRenderer']['title']['runs'][0]['text'])
