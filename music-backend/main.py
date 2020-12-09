ae4d330b253304d6a554671a3f75ab4ca9af46da
Scout2012 <Scout2012>
2020-12-08 21:04:35 -0800

    added error handling for input and indexing

--- music-backend/main.py
+++ music-backend/main.py
@@ -2,25 +2,56 @@ import re
 import sys
 import json
 import requests
-from typing import List
+from datetime import datetime
+from typing import List, Tuple
+
+def find_regex_matches(expression: str, source: str) -> Tuple[str]:
+        pattern = re.compile(expression)
+        return re.findall(pattern, source)
 
 def get_playlist_titles(playlist: str) -> List[str]:
 
     video_names = []
     source = requests.get(playlist).text
-    pattern = re.compile(r'(window["ytInitialData"]|var\s*ytInitialData)\s*=\s*({.+?}.+?);')
-    data = re.findall(pattern, source)[0][1]
 
-    playlist_videos = json.loads(data)['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['playlistVideoListRenderer']['contents']
-
-    for video in playlist_videos:
-        video_names.append(video['playlistVideoRenderer']['title']['runs'][0]['text'])
+    YT_INITIAL_DATA_RE = r'(window["ytInitialData"]|var\s*ytInitialData)\s*=\s*({.+?}.+?);'
+    
+    matched_intial_data = find_regex_matches(YT_INITIAL_DATA_RE, source)
+    try:
+        data = matched_intial_data[0][1]
+        print(data)
+        playlist_videos = json.loads(data)['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['playlistVideoListRenderer']['contents']
+        for video in playlist_videos:
+            video_names.append(video['playlistVideoRenderer']['title']['runs'][0]['text'])
+    except IndexError as e:
+        write_to(str(e), LOG_NAME)
+        print(f'Error when attempting to parse YT_INITIAL_DATA. Please inspect {LOG_NAME}')
 
     return video_names
 
 def find_candidates(seed: List[str]) -> int:
     print(seed)
 
-seed_link = sys.argv[1]
+def write_to(msg: str, filename: str) -> None:
+    ts = datetime.now()
+    f = open(filename, "a")
+    f.write(f'\n{ts} {msg}')
+    f.close()
+
+LOG_NAME = 'log.txt'
+VALID_YOUTUBE_PLAYLIST_LINK_RE = r'https://www.youtube.com/playlist\?list=[^\s]+'
+
+try:
+    seed_link = sys.argv[1]
+except IndexError:
+    print('Missing Youtube playlist parameter')
+    print('Usage: python main.py YOUTUBE_PLAYLIST_LINK')
+    exit()
+
+youtube_link_match = find_regex_matches(VALID_YOUTUBE_PLAYLIST_LINK_RE, seed_link)
+if len(youtube_link_match) != 1:
+    print('Invalid Youtube playlist link')
+    exit()
+
 seed_title = get_playlist_titles(seed_link)
 find_candidates(seed_title)
